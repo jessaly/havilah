6b83386e8bae94b17f94a4aa76171932714b0776
jessaly <jeyajay1997@gmail.com>
2025-05-27 19:55:07 +0000

    another update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
